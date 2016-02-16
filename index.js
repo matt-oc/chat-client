d63edc823cdf9aa67b74928afde361a69bc8a65d
Matthew O'Connor <matthewocsecond@gmail.com>
2016-02-16 17:17:15 +0000

    working version

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var path = require('path');
 
 app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
 });
 
 http.listen(3000, function(){
